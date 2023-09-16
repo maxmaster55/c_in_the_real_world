b623bdfbe0feb556081be1cd9a3f89beab80fd49
maxmaster55 <yousseflkashef000@gmail.com>
2023-09-16 11:27:16 +0300

    removed test ans

--- exercises/ex1/main.c
+++ exercises/ex1/main.c
@@ -5,28 +5,19 @@
 
 
 // model a datatype that represents a Point in 2D space
-typedef struct {
-    float x;
-    float y;
-}point_t;
-
 
 // finish the function (you may change the parametrs) 
-float point_get_distance(point_t p1, point_t p2){
+float point_get_distance(){
 
-    float res = sqrt(pow(p2.x - p1.x, 2)+pow(p2.y - p1.y, 2));
-
-    return res;
+    return PLACEHOLDER;
 }
 
 
 // find the distance between points (1, 6) and (9, 8)
 int main(){
-    point_t p1 = {1, 6};
-    point_t p2 = {9, 8};
 
 
-    printf("distance=%.2f", point_get_distance(p1, p2));
+    printf("distance=%.2f", PLACEHOLDER);
 
     return 0;
 }
